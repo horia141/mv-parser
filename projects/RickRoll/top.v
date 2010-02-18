d666e50c360f5c158602b588df95cc5c6f7c53f2
Horia Coman <horia141@gmail.com>
2010-02-19 00:46:20 +0200

    Refactored the directory structure.
    Added common/ : a directory for common files, used by more projects.
          common/libmv.v : base modules (Reg,Counter etc.).
          projects/ : a directory for individual projects.
          projects/RickRoll : the RickRoll project directory
          projects/Test : the Test project directory

---
+++ projects/RickRoll/top.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+
+module test;
+   reg clock;
+   reg reset;
+
+   initial begin
+      clock = 0;
+      
+      forever #2 clock = ~clock;
+   end
+
+   initial begin
+      #0 reset = 0;
+      #2 reset = 1;
+      #4 reset = 0;
+   end
+   
+   RR_Module rr(clock,reset);
+endmodule // test
+
+   
---
+++ projects/Test/top.v
@@ -0,0 +1,4 @@
+module Top;
+   Data16to32 #(.Size(16))
+   data ();
+endmodule // Top
